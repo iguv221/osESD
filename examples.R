eba47c5b905cb6e133f7e0e3bde6a4d65d99a82c
Sungwoo Kim <iguv7447@hanyang.ac.kr>
2024-06-09 06:31:29 +0900

    changed name

--- examples.R
+++ examples.R
@@ -13,11 +13,12 @@ data(A3Benchmark_TS79)
 data(ARIMA5_quad_2)
 
 osESD_df1 <- A3Benchmark_TS79
+ARIMA_df <- ARIMA5_quad_2
 
 ex_osESD_anoms<-osESD(data=ARIMA_df,size=100)
 ex_osESD_anoms
 
-ARIMA_df <- ARIMA5_quad_2
+
 
 
 
